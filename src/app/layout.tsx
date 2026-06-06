0085e60fc580f809615b0e089f010e48619b2e6c
Piotr Zelazkiewicz <contact@miterion.com>
2026-06-06 11:15:34 +0200

    Poprawka builda: cookie banner, typy globalne i dodanie assetów logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/symbol-miterion.png" type="image/png" />
+      </head>
       <body className={inter.className}>
         <div className="min-h-screen flex flex-col">
           <Header />
