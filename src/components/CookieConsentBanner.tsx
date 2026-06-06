0b58fe25b471f4ff5f6d89f76d9e5d3e2c26b6b0
Piotr Zelazkiewicz <contact@miterion.com>
2026-06-06 13:49:35 +0200

    Refactor CookieConsentBanner for GDPR compliance: Add prior consent logic, Decline button, and persistent localStorage

--- src/components/CookieConsentBanner.tsx
+++ src/components/CookieConsentBanner.tsx
@@ -8,24 +8,59 @@ export default function CookieConsentBanner() {
   const [consentGiven, setConsentGiven] = useState(false);
 
   useEffect(() => {
-    if (localStorage.getItem('CookieConsent') === 'true') {
+    const storedConsent = localStorage.getItem('cookie_consent');
+    if (storedConsent === 'granted') {
       setConsentGiven(true);
     }
   }, []);
 
+  const handleAccept = () => {
+    localStorage.setItem('cookie_consent', 'granted');
+    setConsentGiven(true);
+  };
+
+  const handleDecline = () => {
+    localStorage.setItem('cookie_consent', 'declined');
+  };
+
   return (
     <>
       <CookieConsent
-        onAccept={() => setConsentGiven(true)}
+        onAccept={handleAccept}
+        onDecline={handleDecline}
         location='bottom'
         buttonText='Accept All'
-        cookieName='CookieConsent'
+        declineButtonText='Decline'
+        cookieName='cookie_consent'
       >
-        This website uses cookies. Read more in our <Link href='/privacy-policy'>Cookie Policy</Link>.
+        This website uses cookies to enhance your experience. By clicking "Accept All", you consent to the use of cookies for analytics and marketing purposes. Read more in our <Link href='/privacy-policy'>Privacy Policy</Link>.
       </CookieConsent>
       {consentGiven && (
         <>
+          <Script src='https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID' strategy='afterInteractive' />
+          <Script id='google-analytics' strategy='afterInteractive'>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'GA_MEASUREMENT_ID');
+            `}
+          </Script>
           <Script src='https://connect.facebook.net/en_US/fbevents.js' strategy='afterInteractive' />
+          <Script id='facebook-pixel' strategy='afterInteractive'>
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', 'YOUR_FACEBOOK_PIXEL_ID');
+              fbq('track', 'PageView');
+            `}
+          </Script>
         </>
       )}
     </>
