a3639c8db11d2120261222f51885dbd98da89c78
Piotr Zelazkiewicz <contact@miterion.com>
2026-06-21 01:35:34 +0200

    fix: resolve cal.com script conflict to unblock captcha

--- src/components/CalInitializer.tsx
+++ src/components/CalInitializer.tsx
@@ -1,13 +1,16 @@
-﻿'use client';
+'use client';
 import Script from 'next/script';
 
 export default function CalInitializer() {
   return (
     <Script
-      id="cal-preload"
       src="https://cal.com/embed.js"
       strategy="afterInteractive"
-      onLoad={() => {(window as any).Cal("init", {origin: "https://cal.com"});}}
+      onLoad={() => {
+        if (!(window as any).Cal) {
+          (window as any).Cal("init", { origin: "https://cal.com" });
+        }
+      }}
     />
   );
 }
