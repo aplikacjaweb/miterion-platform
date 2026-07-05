abdc1dd3a80d658c4547f7ef0ad491021772e6d8
Piotr Zelazkiewicz <contact@miterion.com>
2026-07-05 23:09:13 +0200

    fix: adjust next config external packages for next 14.1.0 and puppeteer syntax

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    // Wymuszenie zachowania plików binarnych na Vercelu
     outputFileTracingIncludes: {
       '/api/**/*': ['./node_modules/@sparticuz/chromium/bin/*'],
     },
+    // Wersja 14.1.0 wymaga tej nazwy wewnątrz experimental, aby wykluczyć paczki z kompilacji Webpacka
+    serverComponentsExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
   },
-  serverExternalPackages: ['@sparticuz/chromium'],
 };
 
 module.exports = nextConfig;
